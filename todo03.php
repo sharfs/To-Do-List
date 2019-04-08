6160b0c384b42f145caa8204db853eb2ca129b1f
Sharfaa Anthony <sharfaasedick@gmail.com>
2019-04-08 14:32:21 +0200

    Styled very briefly using css

--- todo03.php
+++ todo03.php
@@ -12,6 +12,7 @@ if($_POST){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="style.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
@@ -20,7 +21,7 @@ if($_POST){
 <div>
   <h2>My To-Do List:</h3><br>
 <input type="text" name="todoEntry" id="todoEntry">
-<input type="submit" value="add">
+<input type="submit" value="Add">
 </div>
 </form>
 
@@ -66,6 +67,7 @@ $(document).ready(function(){
     if(sessionStorage.getItem(i)==1){
       $(this).css("text-decoration","line-through");
       $(this).css("color", "red");
+      $(this).css("cursor", "pointer");
 }
  });
 
